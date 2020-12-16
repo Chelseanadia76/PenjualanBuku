9fb415f57ae9419eea947e2152cf4119ea467bf9
ACER <ACER@LAPTOP-RTA3BRVG>
2020-12-17 00:42:43 +0800

    new commit

---
+++ src/penjualanbuku/PenjualanBuku.java
@@ -0,0 +1,28 @@
+
+package penjualanbuku;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class PenjualanBuku {
+    private static Connection konek;
+    
+    public static Connection getConnection() throws SQLException{
+        if(konek == null) {
+            new Driver();
+            konek = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/tokobuku","root","");
+        }
+        return konek;
+    }
+    
+    public static void main(String[] args) {
+        try {
+            getConnection();
+            System.out.println("Koneksi Berhasil");
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }    
+}
